e264bbf9927a9a40cd405a48288b813331daca74
Sunil Tanwar <suniltanwar@live.com>
2016-04-15 16:35:58 +0530

    reset password api change

--- code/application/controllers/api.php
+++ code/application/controllers/api.php
@@ -124,6 +124,7 @@ class api extends CI_Controller {
 
     public function resetPassword() {
         $checkAuthToken = FALSE;
+        $result = $this->checkAuth($checkAuthToken);
         if ($result['status'] == 0 && $result['config_status'] != 1) {
             $json_data = json_encode($result);
             $data['response'] = $json_data;
