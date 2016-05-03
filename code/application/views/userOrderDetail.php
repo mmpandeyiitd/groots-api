f7aae1c8f7b9a89c46d868861e9c3f8ee2bcecc7
Sunil Tanwar <suniltanwar@live.com>
2016-05-03 18:45:35 +0530

    order template

--- code/application/views/userOrderDetail.php
+++ code/application/views/userOrderDetail.php
@@ -1,98 +1,59 @@
 <?php
-$message = '<meta http-equiv="Content-Type" c="Cocoa HTML Writer">
-<meta name="CocoaVersion" content="1265">
-<style type="text/css">
-ontent="text/html; charset=utf-8"> <meta http-equiv="Content-Style-Type" content="text/css"> <title></title> <meta name="Generator" content p.p1 {
-margin: 0.0px 0.0px 0.0px 0.0px;
-font: 11.0px Menlo;
-color: #bb2ca2
-}
-body { margin:0; padding:0;}
-p.p2 {
-	margin: 0.0px 0.0px 0.0px 0.0px;
-	font: 12.0px Helvetica
-}
-p.p3 {
-	margin: 0.0px 0.0px 0.0px 0.0px;
-	font: 12.0px Helvetica;
-	min-height: 14.0px
-}
-span.s1 {
-	color: #000000
-}
-span.s2 {
-	color: #967e34
-}
-span.s3 {
-	color: #d12f1b
-}
-</style>
-<link href="http://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
-</head><body>
-<div style="color:#333333;font-size:13px;width:100%; font-family:arial,tahoma,verdana,sans-serif;margin:10px auto;min-height:100%; margin-top: 0; ">  <table width="100%" cellpadding="0" cellspacing="0" style="color:#333;font-size:13p;  font-family: "Roboto", sans-serif; border: 1px solid rgb(216, 216, 216);width: 600px; margin: 0px auto;">
+$message = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">  
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Email Verification </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,600,300italic,700,700italic" rel="stylesheet" type="text/css">
+  </head>
+  <body style="margin: 0; padding: 0; font-family: sans-serif;">
+   <table align="center"  cellpadding="0" border="0" cellspacing="0" width="600" style="border-collapse: collapse; display: block; border:0; background:#fff; ">
     <tbody>
-      <tr>
-        <td valign="top" align="center" style="margin:0;padding:0"><table style="width:600px;border:0px none" width="600" cellpadding="0" cellspacing="0">
-            <tbody>
-              <tr>
-                <td valign="middle" width="53%" height="40" style="padding:10px;color:#ffffff;background-color: #E0E0E0;" bgcolor="015287"><a style="text-decoration:none;outline:none;color:#ffffff;font-size:15px" href="http://www.supplified.com" target="_blank"> <img src="http://commerce.canbrand.in/images/can.png" alt="CANBRAND"> </a></td>
-                
-                <td valign="middle" align="left" width="44%" height="40" style="padding:0 15px 0 5px;  background-color: #E0E0E0;" bgcolor="005387"><a style="text-decoration:none;outline:none; display:block; text-align:center;" href="#" > <span style="font-size:16px;color:rgb(0, 96, 172);line-height:18px;">CUSTOMER SUPPORT</span><br>
-                  <span style="font-size:16px;color:rgb(231, 27, 52);line-height:18px">+91 9999999995</span> </a></td>
-              </tr>
-              <tr>
-                <td colspan="9" align="center" valign="top"><table width="100%" style="border:1px solid #e6e6e6;width:100%" cellpadding="0" cellspacing="0">
-                    <tbody>
-                      <tr>
-                        <td colspan="2" align="left" valign="top" width="100%" style="border-bottom:1px solid #e6e6e6;margin:0;padding:15px;background-color:#f9f9f9" bgcolor="F9F9F9"><p style="padding:0 0 0 0;margin:0;font-size:16px;font-weight:bold"> Hi <a href="#" target="_blank" style=" text-decoration: none;   color: #D10002; ">'.$name.'</a>, </p>
-                          <p style="padding:10px 0;margin:0;color:#565656"> Thank you for your <span class="il">order</span>!</p></td>
-                      </tr>
-                      <tr> 
-                      <td colspan="9">
-<table width="100%" border="0" style="border-collapse:collapse;">
-<tr>
+    <tr style="display: block; ">
+      <td style="padding:0px; width: 150px; background-color: #444;" >
+        <a href="javascript:void(0);" style="display:block; height:63px; "><img src="logo.png" alt="" style="    width: 50px;
+    margin: 8px 20px;"></a>
+      </td>
+      <td style="padding: 5px 10px; width:450px; background-color:#444;color: #fff;font-size: 24px; text-transform: uppercase; text-align:right;">
+        <span style="float:right;">+91 99999 99999</span>
+        <img src="callIco-head.png" alt="call" width="25" style="float:right; margin:0 10px;"> 
+      </td>
+    </tr>
+    <tr style=" width: 600px; display: block;">
+     <td style="display: block; padding: 10px;">
+      <p style="font-size:13px;">
+            <strong>'.$name.',</strong> <br> <br> 
+     Thank you for your order! . Your order ID is '.$order_number.'.
+    <br> <br> 
+    We will send you another email once the items in your order have been shipped.
+      </p>
+     </td>          
+   </tr>
     
-    <td align="left" colspan="4" style="line-height:32px; padding-left: 10px;"><p> Please find below, the summary of your <span>order</span> <span style="color:#666">'.$order_number.'</span> </p></td>
-  </tr>
-  <tr style="background-color: rgb(0, 96, 172);color: rgb(255, 255, 255);">
-  <td style="text-align: center;padding: 8px;border-right: 1px solid #fff; width:50px;">S. No.</td>
-    <td style="text-align: center;padding: 8px;border-right: 1px solid #fff; width:150px;">SELLER</td>
-    <td style="text-align: center;padding: 8px;border-right: 1px solid #fff; width:250px;">ITEM</td>
-    <td style="text-align: center;padding: 8px;border-right: 1px solid #fff; width:50px;">QTY</td>
-    <td style="text-align: center;padding: 8px;border-right: 1px solid #fff; width:100px;">SUBTOTAL</td>
-  </tr>
- '.$product.'
-</table>
-<td>
-     
-                      </tr>
-                      <tr>'.$address.'
-                       
-                      </tr>
-                      <tr>
-                        <td align="left" valign="top" width="50%" style="text-align:left;padding:15px 0;border-top:#e6e6e6 solid 1px;background-color:#f9f9f9" bgcolor="F9F9F9"><div style="padding:15px 10px 0 15px;margin:0;text-align:left;border-right:dashed 1px #b3b3b3">
-                            <p style="padding:0;margin:0 0 7px 0;font-size:16px;color:#565656">What Next?</p>
-                            <p style="padding:0;margin:0;font-size:11px;color:#565656;line-height:20px"> You will receive a phone call on the contact number you provided for order confimation.</p>
-                          </div></td>
-                        <td align="left" valign="top" width="50%" style="text-align:left;padding:15px 0;border-top:#e6e6e6 solid 1px;background-color:#f9f9f9" bgcolor="F9F9F9"><div style="padding:15px 50px 0 20px;margin:0;text-align:left; width:100%;">
-                            <p style="padding:0;margin:0 0 7px 0;font-size:16px;color:#565656">Any Questions?</p>
-                            <p style="padding:0;margin:0;font-size:11px;color:#565656;line-height:20px">You can always reach us at<a href="tel:+919999999995" value="+919999999995" target="_blank"> +91 9999999995</a><br>
-                              or write to us at <a style="text-decoration:underline" href="" target="_blank"><span style="color:#666"><a href="mailto:contactus@supplified.com">contactus@supplified.com</a></span></a></p>
-                          </div></td>
-                      </tr>
-                      <tr>
-                        <td align="center" valign="top" style="padding:15px 40px;margin:0;text-align:center;background-color:#f9f9f9" colspan="2" bgcolor="F9F9F9"><p style="padding:0;margin:0 0 7px 0"> <a title="" style="text-decoration:none;color:#565656" href="" target="_blank"><span style="color:#565656">supplified.com</span></a> </p>
-                          <p style="padding:10px 0 0 0;margin:0;border-top:solid 1px #cccccc;font-size:11px;color:#565656"> In case you need any help feel free to contact us at <a style="text-decoration:underline" href="" target="_blank"><span style="color:#666"><a href="mailto:contactus@supplified.com">contactus@supplified.com</a></p></td>
-                      </tr>
-                    </tbody>
-                  </table></td>
-              </tr>
-            </tbody>
-          </table></td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-</body>
+  
+     <tr style=" width: 600px; display: block;  margin: 20px 0px 0px;  background: #F3F3F3; padding: 10px 0;">
+    <td style="  width: 300px; " >
+      '.$address.'
+     </td>  
+                
+   </tr>
+    <tr style="display: block; margin-top:0px;background: #444; padding: 15px 0;">
+      <td colspan="2" style="width: 600px;">
+        <ul style="display:block; width:100%; list-style-type:none; overflow: hidden;margin: 0;padding: 10px 0;">
+          <li style="display:block; width:200px; float:left; text-align:center;">
+            <a href="#!" style="display:block;color:#a9a9a9; text-transform:uppercase;text-decoration:none; font-size:14px; border-right:1px solid #676767;">Visit Website</a>
+          </li>
+          <li style="display:block; width:200px; float:left; text-align:center;">
+            <a href="#!" style="display:block;color:#a9a9a9; text-transform:uppercase;text-decoration:none; font-size:14px;">Terms &amp; Conditions</a>
+          </li>
+          <li style="display:block; width:200px; float:left; text-align:center;">
+            <a href="#!" style="display:block;color:#a9a9a9; text-transform:uppercase;text-decoration:none; font-size:14px; border-left:1px solid #676767;">Privacy Policy</a>
+          </li>
+        </ul>
+      </td> 
+    </tr>
+  </tbody></table>
+  </body>
 </html>';
 echo $message ;
