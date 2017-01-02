cd9478891d8a7ba3c93f7df5d4489ad8f9b444c3
ashu dogra <ashudogra20@gmail.com>
2017-01-02 20:02:07 +0530

    -1hour

--- code/application/controllers/api.php
+++ code/application/controllers/api.php
@@ -395,7 +395,7 @@ class api extends CI_Controller {
         $result['msg'] = "Server Date Time";
         $result['errors'] = array();
         $date = date("Y-m-d H:i:s");
-        $date = date('Y-m-d H:i:s', strtotime($date.' + 1 hour'));
+        $date = date('Y-m-d H:i:s', strtotime($date.' - 1 hour'));
         $result['data']['current_date_time'] = $date;
         $this->output->set_header('AUTH_TOKEN:'.$this->authToken);
         $this->returnfunction($result);
