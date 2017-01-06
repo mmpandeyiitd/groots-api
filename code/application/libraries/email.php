e91ae252ad5d01b79be5b63ecebecd77a37a50bd
manmohan <mmpandeyiitd@gmail.com>
2017-01-06 14:51:14 +0530

    delete aws folder and email lib

--- code/application/libraries/email.php
+++
@@ -1,8 +0,0 @@
-<?php
- if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
-class email {
- public function __construct() { 
- 	require_once APPPATH.'third_party/aws/aws-autoloader.php'; 
- } 
-}
